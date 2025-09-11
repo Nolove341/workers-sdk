87432ec922ac2ccc7d20fb819bca8cbaeb32d7e4
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-09-11 12:43:41 -0700

    fix tests
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- packages/wrangler/src/config/validation.ts
+++ packages/wrangler/src/config/validation.ts
@@ -2763,7 +2763,7 @@ function validateContainerApp(
 					"class_name",
 					"scheduling_policy",
 					"instance_type",
-					"ssh",
+					"wrangler_ssh",
 					"authorized_keys",
 					"configuration",
 					"constraints",
@@ -2793,7 +2793,7 @@ function validateContainerApp(
 
 				if (
 					!isRequiredProperty(
-						containerAppOptional.wrangler_ssh.enabled,
+						containerAppOptional.wrangler_ssh,
 						"enabled",
 						"boolean"
 					)
@@ -2805,7 +2805,7 @@ function validateContainerApp(
 
 				if (
 					!isOptionalProperty(
-						containerAppOptional.wrangler_ssh.port,
+						containerAppOptional.wrangler_ssh,
 						"port",
 						"number"
 					) ||
@@ -2831,7 +2831,7 @@ function validateContainerApp(
 				if (!Array.isArray(containerAppOptional.authorized_keys)) {
 					diagnostics.errors.push(`${field}.authorized_keys must be an array`);
 				} else {
-					for (const item of containerAppOptional.authorized_keys) {
+					for (const item in containerAppOptional.authorized_keys) {
 						const fieldPath = `${field}.authorized_keys[${item}]`;
 						const key = containerAppOptional.authorized_keys[item];
 
@@ -2852,7 +2852,7 @@ function validateContainerApp(
 							);
 						}
 
-						if (key.public_key.toLowerCase().startsWith("ssh-ed25519")) {
+						if (!key.public_key.toLowerCase().startsWith("ssh-ed25519")) {
 							diagnostics.errors.push(
 								`${fieldPath}.public_key is a unsupported key type. Please provide a ED25519 public key.`
 							);
